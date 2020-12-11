74be4e1bfae499d22a870375d5d81a1f7bcacc88
Maria Fernanda Ferreira Alfaro <a2018114425@estudiantes.upsa.edu.bo>
2020-12-11 16:18:09 -0400

    Se creo el data context

---
+++ PreparatoriaFInal/Models/Final.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PreparatoriaFInal.Models
+{
+    public class Final
+    {
+        [Key]
+        public int numero { get; set; }
+    }
+}
